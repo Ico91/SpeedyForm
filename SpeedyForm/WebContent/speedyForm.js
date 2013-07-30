5f1f768f84c6e3d3c7e22412d46da26aff84f17a
Ico91 <hristo_danovski2@abv.bg>
2013-07-30 20:17:01 +0300

    Update speedyForm.js

--- SpeedyForm/WebContent/speedyForm.js
+++ SpeedyForm/WebContent/speedyForm.js
@@ -1,58 +1,48 @@
-var jsonData;
-
 function SpeedyAddress() {
 	this.display = function(elementId) {
-		var divContainer = document.createElement('div');
-		divContainer.setAttribute('id', 'formContainer');
-		document.getElementById(elementId).appendChild(divContainer);
-		var label = document.createElement('label');
-		label.setAttribute('for', 'myInput');
-		label.setAttribute('id', 'myLabel');
-		label.innerHTML = 'Write a country: ' + '<br/>';
-		document.getElementById("formContainer").appendChild(label);
-		var input = document.createElement('input');
-		input.setAttribute('name', 'myInput');
-		input.setAttribute('id', 'myInput');
-		input.setAttribute('type', 'text');
-		document.getElementById("formContainer").appendChild(input);
-		var btn = document.createElement('button');
-		btn.setAttribute('type', 'submit');
-		btn.setAttribute('id', 'myBtn');
-		btn.setAttribute('onClick', 'myFunc()');
-		btn.innerHTML = 'Click me!';
-		document.getElementById("formContainer").appendChild(btn);
-		var script = document.createElement('script');
-		script.setAttribute('type', 'text/javascript');
-		script.innerHTML = getScriptBody() + '\n' + '$(document).ready(function(){  });';
-		document.getElementById("formContainer").appendChild(script);
+		
+		_fillHtml(elementId);
+		
+		_setupUI("countries", "inputCountry");
+		_setupUI("cities", "inputCity");
+		
+		function _setupUI(pageURL, containerID) {
+			$('#' + containerID).autocomplete({
+				  source: function( request, response ) {
+				        $.ajax({
+				          url: "http://192.168.1.100:9999/" + pageURL,
+				          dataType: "jsonp",
+				          data: {
+				            search: request.term
+				          },
+				          success: function( data ) {
+				            response( $.map( data, function( item ) {
+				              return {
+				                label: item.name,
+				                value: item.name
+				              };
+				            }));
+				          }
+				        });
+				     },
+					minLength: 3 
+			});
+		}
+		
+		function _fillHtml(elementId) {
+			var html = '<div id="formContainer">' + 
+							'<label for="inputCountry">Country:</label>' +
+							'<input type="text" name="inputCountry" id="inputCountry"></br>' +
+							'<label for="inputCity">City:</label>' +
+							'<input type="text" name="inputCity" id="inputCity">' +
+						'</div>';
+			
+			$('#'+elementId).append(html);
+			
+			$('head').append('<link rel=\'stylesheet\' type=\'text/css\' href=\'http://localhost:8080/SpeedyForm/style.css\'/>');
+			$('head').append('<link rel=\'stylesheet\' type=\'text/css\' href=\'http://localhost:8080/SpeedyForm/jquery-ui.min.css\'/>');
+
+		}
+		
 	};
 };
-
-function getScriptBody() {
-	scriptBody = 'var jsonData = [];' +
-		'$(document).ready(function(){' + 
-			'$.ajax({' + 
-				'url: \'http://192.168.1.100:9999/countries?callback=?\',' +
-				'type: \'GET\', ' +
-				'jsonpCallback: \'callback\',' +
-			    'contentType: \"application/json\",' +
-			    'dataType: \'jsonp\',' +
-				'success: function(data) { ' +
-					'for(var i = 0; i < data.length; i++) {'+
-						'	jsonData[i] = data[i].name' +
-						'}' +
-					'$(\'#myInput\').autocomplete({' +
-						'source: jsonData ' +
-						','+
-						'minLength: 3 });' +
-				'}, ' +
-				'error: function(err, textStatus, errorThrown) {' +
-					'alert("Server not found");' +
-			'}}); ' +
-		'});';
-	return scriptBody;
-}
-
-function myFunc() {
-	console.log(jsonData);
-}
