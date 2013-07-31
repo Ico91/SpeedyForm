196e878be41d196e387954d9d15289fdbbbaf40d
Ico91 <hristo_danovski2@abv.bg>
2013-07-31 12:10:35 +0300

    Return json

--- SpeedyForm/WebContent/speedyForm.js
+++ SpeedyForm/WebContent/speedyForm.js
@@ -9,12 +9,19 @@ function SpeedyAddress() {
 		function _setupUI(pageURL, containerID) {
 			$('#' + containerID).autocomplete({
 				  source: function( request, response ) {
+					  var param;
+					  if(containerID === 'inputCity')
+						  param =  {
+					        	  country: $("#inputCountry").val(),
+					        	  search: request.term
+					          };
+					  else
+						  param = {  search: request.term };
+					  
 				        $.ajax({
 				          url: "http://192.168.1.100:9999/" + pageURL,
 				          dataType: "jsonp",
-				          data: {
-				            search: request.term
-				          },
+				          data: param,
 				          success: function( data ) {
 				            response( $.map( data, function( item ) {
 				              return {
@@ -22,7 +29,7 @@ function SpeedyAddress() {
 				                value: item.name
 				              };
 				            }));
-				          }
+				          },
 				        });
 				     },
 					minLength: 3 
@@ -31,11 +38,20 @@ function SpeedyAddress() {
 		
 		function _fillHtml(elementId) {
 			var html = '<div id="formContainer">' + 
-							'<label for="inputCountry">Country:</label>' +
-							'<input type="text" name="inputCountry" id="inputCountry"></br>' +
-							'<label for="inputCity">City:</label>' +
-							'<input type="text" name="inputCity" id="inputCity">' +
-						'</div>';
+					       '<table>' + 
+					       	'<tr>' +
+						        '<td><label for="inputCountry">Country:</label></td>' +
+						        '<td><input type="text" name="inputCountry" id="inputCountry"></td>' +
+						    '</tr>' +
+						    '<tr>' +
+						        '<td><label for="inputCity">City:</label></td>' +
+						        '<td><input type="text" name="inputCity" id="inputCity"></td>' +
+						    '</tr>' + 
+						    '<tr colspan = "2">' +
+						    	'<td><button type="submit" id="submitForm">Submit</button></td>' +
+						    '</tr>' +
+						    '</table>' +
+				      '</div>';
 			
 			$('#'+elementId).append(html);
 			
@@ -46,3 +62,11 @@ function SpeedyAddress() {
 		
 	};
 };
+
+function formData() {
+	var jsonObject;
+
+	jsonObject = { 'Country' : $('#inputCountry').val(), 'City' : $('#inputCity').val() };
+
+	return JSON.stringify(jsonObject);
+}
